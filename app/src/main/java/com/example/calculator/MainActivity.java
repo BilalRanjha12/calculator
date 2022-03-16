c653f9395196a34595dc741f44ffda4e7a9ca671
Zeeshan Umer <zumer@leeddev.io>
2022-03-16 13:16:13 +0500

    check commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //hjkhjhjjkjhbk
-        //zeeshan
+        //zeeshan//fghj
         setContentView(R.layout.activity_main);
       //bilal changes are here
     }
