1a92424d8c2bdfc5a2026c893bfd5229fcabd184
Zeeshan Umer <zumer@leeddev.io>
2022-03-16 13:09:36 +0500

    zeeshan changes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //hjkhjhjjkjhbk
+        //zeeshan
         setContentView(R.layout.activity_main);
       //bilal changes are here
     }
